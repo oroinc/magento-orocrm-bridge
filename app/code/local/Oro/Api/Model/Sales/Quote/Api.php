c8bb164562e8158c219db91fe778690bb2003852
Ivan Shakuta <ishakuta@gmail.com>
2013-12-05 13:26:43 +0200

    CRM-654 Cart items connector, reader and strategy
    include shipping address, billing address, quote items and payment info in quote list

--- app/code/local/Oro/Api/Model/Sales/Quote/Api.php
+++ app/code/local/Oro/Api/Model/Sales/Quote/Api.php
@@ -11,8 +11,10 @@ class Oro_Api_Model_Sales_Quote_Api
     {
         /** @var Mage_Sales_Model_Resource_Quote_Collection $quoteCollection */
         $quoteCollection = Mage::getResourceModel('sales/quote_collection');
+
         /** @var $apiHelper Mage_Api_Helper_Data */
         $apiHelper = Mage::helper('api');
+
         $filters = $apiHelper->parseFilters($filters, $this->_attributesMap['quote']);
         try {
             foreach ($filters as $field => $value) {
@@ -22,8 +24,45 @@ class Oro_Api_Model_Sales_Quote_Api
             $this->_fault('filters_invalid', $e->getMessage());
         }
 
-        $arr = $quoteCollection->toArray();
+        $resultArray = [];
+        foreach ($quoteCollection as $quote) {
+            $resultArray[] = array_merge($quote->__toArray(), $this->info($quote));
+        }
 
-        return $arr['items'];
+        return $resultArray;
+    }
+
+    /**
+     * Retrieve full information about quote
+     *
+     * @param  $quote
+     * @return array
+     */
+    protected function info($quote)
+    {
+        if ($quote->getGiftMessageId() > 0) {
+            $quote->setGiftMessage(
+                Mage::getSingleton('giftmessage/message')->load($quote->getGiftMessageId())->getMessage()
+            );
+        }
+
+        $result = $this->_getAttributes($quote, 'quote');
+        $result['shipping_address'] = $this->_getAttributes($quote->getShippingAddress(), 'quote_address');
+        $result['billing_address'] = $this->_getAttributes($quote->getBillingAddress(), 'quote_address');
+        $result['items'] = array();
+
+        foreach ($quote->getAllItems() as $item) {
+            if ($item->getGiftMessageId() > 0) {
+                $item->setGiftMessage(
+                    Mage::getSingleton('giftmessage/message')->load($item->getGiftMessageId())->getMessage()
+                );
+            }
+
+            $result['items'][] = $this->_getAttributes($item, 'quote_item');
+        }
+
+        $result['payment'] = $this->_getAttributes($quote->getPayment(), 'quote_payment');
+
+        return $result;
     }
 }
