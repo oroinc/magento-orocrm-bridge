a066048e44a1e7cf604480e4b0a94e30b72dc9aa
sergey <sergey@cherepanov.org.ua>
2013-12-05 14:21:50 +0200

    CRM-618 Product list update

---
+++ app/code/local/Oro/Api/Model/Catalog/Product/Api.php
@@ -0,0 +1,46 @@
+<?php
+
+class Oro_Api_Model_Catalog_Product_Api
+    extends Mage_Catalog_Model_Product_Api
+{
+    /**
+     * Retrieve list of products with basic info (id, sku, type, set, name)
+     *
+     * @param null|object|array $filters
+     * @param string|int $store
+     * @return array
+     */
+    public function items($filters = null, $store = null)
+    {
+        /** @var Mage_Catalog_Model_Resource_Product_Collection $collection */
+        $collection = Mage::getModel('catalog/product')->getCollection();
+        $collection->addStoreFilter($this->_getStoreId($store));
+        $collection->addAttributeToSelect(array('name', 'price', 'special_price'));
+
+        /** @var $apiHelper Mage_Api_Helper_Data */
+        $apiHelper = Mage::helper('api');
+        $filters = $apiHelper->parseFilters($filters, $this->_filtersMap);
+        try {
+            foreach ($filters as $field => $value) {
+                $collection->addFieldToFilter($field, $value);
+            }
+        } catch (Mage_Core_Exception $e) {
+            $this->_fault('filters_invalid', $e->getMessage());
+        }
+        $result = array();
+        foreach ($collection as $product) {
+            $result[] = array(
+                'product_id' => $product->getId(),
+                'sku'        => $product->getSku(),
+                'name'       => $product->getName(),
+                'set'        => $product->getAttributeSetId(),
+                'type'       => $product->getTypeId(),
+                'category_ids' => $product->getCategoryIds(),
+                'website_ids'  => $product->getWebsiteIds(),
+                'price'  => $product->getPrice(),
+                'special_price'  => $product->getSpecialPrice(),
+            );
+        }
+        return $result;
+    }
+}
