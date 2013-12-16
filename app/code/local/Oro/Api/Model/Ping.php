7b7b807e158f480739f328cba2ac7a53068690c3
Ivan Shakuta <ishakuta@gmail.com>
2013-12-16 18:32:18 +0000

    Add oroPing method to return: - extension vertion, - mage version - mage edition (CE/EE)

---
+++ app/code/local/Oro/Api/Model/Ping.php
@@ -0,0 +1,17 @@
+<?php
+
+class Oro_Api_Model_Ping extends Mage_Api_Model_Resource_Abstract
+{
+    const VERSION = '1.0.0';
+    /**
+     * @return array
+     */
+    public function ping()
+    {
+        return array(
+            'version'      => self::VERSION,
+            'mage_version' => Mage::getVersion(),
+            'mage_edition' => Mage::getEdition(),
+        );
+    }
+}
