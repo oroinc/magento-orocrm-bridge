c8f374b0dd14a1f44cb6ed149369a35b19301eb8
sergey <sergey@cherepanov.org.ua>
2013-12-24 21:08:44 +0200

    CRM-618 Add tests config.sh

--- app/code/community/Oro/Api/Model/Catalog/Product/Api/V2.php
+++ app/code/community/Oro/Api/Model/Catalog/Product/Api/V2.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * MageCore
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is published at http://opensource.org/licenses/osl-3.0.php.
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magecore.com so we can send you a copy immediately
+ *
+ * @category Oro
+ * @package Api
+ * @copyright Copyright 2013 Oro Inc. (http://www.orocrm.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
 class Oro_Api_Model_Catalog_Product_Api_V2
     extends Mage_Catalog_Model_Product_Api_V2
 {
