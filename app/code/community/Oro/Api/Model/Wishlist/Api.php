4bfd27642e061a8b834cc8acf17ecb1f984928d8
Olga Syzova <osizova@magecore.com>
2016-02-01 14:53:43 +0200

    CRM-4990: Extend Magento OroCRM Bridge extension to sync wishlists per website

--- app/code/community/Oro/Api/Model/Wishlist/Api.php
+++ app/code/community/Oro/Api/Model/Wishlist/Api.php
@@ -15,21 +15,28 @@
  * @copyright Copyright 2013 Oro Inc. (http://www.orocrm.com)
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class Oro_Api_Model_Wishlist_Api
-    extends Mage_Checkout_Model_Api_Resource
+
+class Oro_Api_Model_Wishlist_Api extends Mage_Checkout_Model_Api_Resource
 {
     protected $_mapAttributes = array(
         'wishlist_id' => 'entity_id'
     );
 
     /**
+     * Store ids (website stores)
+     *
+     * @var array
+     */
+    protected $storeIds = null;
+
+    /**
      * @var Oro_Api_Helper_Data
      */
-    protected $_apiHelper;
+    protected $apiHelper;
 
     public function __construct()
     {
-        $this->_apiHelper = Mage::helper('oro_api');
+        $this->apiHelper = Mage::helper('oro_api');
     }
 
     /**
@@ -41,7 +48,7 @@ class Oro_Api_Model_Wishlist_Api
     {
         /** @var Mage_Wishlist_Model_Resource_Wishlist_Collection $collection */
         $collection = Mage::getResourceModel('wishlist/wishlist_collection');
-        $filters = $this->_apiHelper->parseFilters($filters);
+        $filters = $this->apiHelper->parseFilters($filters);
         try {
             foreach ($filters as $field => $value) {
                 $collection->addFieldToFilter($field, $value);
@@ -50,7 +57,7 @@ class Oro_Api_Model_Wishlist_Api
             $this->_fault('filters_invalid', $e->getMessage());
         }
 
-        if ($pager && !$this->_apiHelper->applyPager($collection, $pager)) {
+        if ($pager && !$this->apiHelper->applyPager($collection, $pager)) {
             // there's no such page, so no results for it
             return array();
         }
@@ -65,34 +72,37 @@ class Oro_Api_Model_Wishlist_Api
      *
      * @param array|object   $filters
      * @param null|\stdClass $pager
-     * @param int|null $websiteId
      * @return array
      *
      * @throws Mage_Api_Exception
      */
-    public function listWithItems($filters, $pager = null, $websiteId = null)
+    public function listWithItems($filters, $pager = null)
     {
         /** @var Mage_Wishlist_Model_Resource_Wishlist_Collection $collection */
         $collection = Mage::getResourceModel('wishlist/wishlist_collection');
-        $filters = $this->_apiHelper->parseFilters($filters);
+        $filters = $this->apiHelper->parseFilters($filters, array('updated_at' => 'main_table.updated_at'));
+        /* Prepare store/website filters for wishlist  */
+        $filters = $this->prepareFilters($filters);
+
         try {
             foreach ($filters as $field => $value) {
                 $collection->addFieldToFilter($field, $value);
             }
+            // Load customer data
+            $collection = $this->addCustomerData($collection);
         } catch (Mage_Core_Exception $e) {
             $this->_fault('filters_invalid', $e->getMessage());
         }
 
-        if ($pager && !$this->_apiHelper->applyPager($collection, $pager)) {
+        if ($pager && !$this->apiHelper->applyPager($collection, $pager)) {
             // there's no such page, so no results for it
             return array();
         }
 
         $result = array();
-        $storeIds = $this->getStoreIds($websiteId);
         foreach ($collection as $wishlist) {
             /** @var Mage_Wishlist_Model_Wishlist $wishlist */
-            $wishlistData = $this->info($wishlist, $storeIds);
+            $wishlistData = $this->info($wishlist);
             $result[] = $wishlistData;
         }
 
@@ -100,28 +110,64 @@ class Oro_Api_Model_Wishlist_Api
     }
 
     /**
+     * Retrieve store ids
+     *
+     * @param null|integer $websiteId
+     * @return array
+     */
+    public function getStoreIds($websiteId = null)
+    {
+        $storeIds = array();
+        if ($websiteId) {
+            $website = Mage::getModel('core/website')->load($websiteId);
+            $storeIds = $website->getStoreIds();
+        } else {
+            $stores = Mage::app()->getStores();
+            foreach ($stores as $store) {
+                $storeIds[] = $store->getId();
+            }
+        }
+        return $storeIds;
+    }
+
+    /**
+     * Add customer data to collection
+     *
+     * @param Mage_Wishlist_Model_Resource_Wishlist_Collection $collection
+     * @return Mage_Wishlist_Model_Resource_Wishlist_Collection
+     */
+    protected function addCustomerData($collection)
+    {
+        $customerTable = $collection->getTable('customer/entity');
+        $select = $collection->getSelect();
+        $select->joinLeft(
+            array('customer' => $customerTable),
+            'main_table.customer_id = customer.entity_id',
+            array(
+                'customer_email' => 'email',
+                'website_id' => 'website_id'
+            )
+        );
+
+        return $collection;
+    }
+
+    /**
      * Retrieve full information about wishlist
      *
      * @param Mage_Wishlist_Model_Wishlist $wishlist
-     * @param array $storeIds
      * @return array
      */
-    protected function info($wishlist, $storeIds)
+    protected function info($wishlist)
     {
-        // Load customer data
-        $customer = Mage::getModel('customer/customer')
-            ->load($wishlist->getCustomerId());
-        if ($customer) {
-            $wishlist->setCustomerEmail($customer->getEmail());
-        }
         $result = $wishlist->toArray();
         $result['items'] = array();
 
-        //Load wishlist items data
+        // Load wishlist items data
         /** @var Mage_Wishlist_Model_Resource_Item_Collection $collection */
         $collection = Mage::getResourceModel('wishlist/item_collection')
             ->addWishlistFilter($wishlist)
-            ->addStoreFilter($storeIds)
+            ->addStoreFilter($this->storeIds)
             ->setVisibilityFilter();
 
         /** @var Mage_Wishlist_Model_Item $item */
@@ -139,23 +185,56 @@ class Oro_Api_Model_Wishlist_Api
     }
 
     /**
-     * Retrieve store ids for website or all stores if $websiteId is not send
+     * Prepare store/website filters for wishlist
      *
-     * @param int $websiteId Use website if exist
+     * Exclude store fields from complex filter for wishlist if exists
+     * Save store filter data to storeIds - used for filter wishlist items
+     * Save website filter data to websiteIds - used for filter wishlists by customer's website
+     *
+     * Example if complex filter:
+     * 'website_id',
+     * [
+     *     'key'   => 'website_id',
+     *     'value' => [
+     *         'key'   => 'eq',
+     *         'value' => '1'
+     *      ]
+     * ]
+     * 'store_id',
+     * [
+     *     'key' => 'store_id',
+     *     'value' => [
+     *         'key' => 'in',
+     *         'value' => [1,2]
+     *     ]
+     * ]
+     *
+     * @param  array $filters
      * @return array
      */
-    public function getStoreIds($websiteId = null)
+    protected function prepareFilters($filters)
     {
-        $storeIds = array();
-        if ($websiteId) {
-            $website = Mage::getModel('core/website')->load($websiteId);
-            $storeIds = $website->getStoreIds();
-        } else {
-            $stores = Mage::app()->getStores();
-            foreach ($stores as $store) {
-                $storeIds[] = $store->getId();
+        if (isset($filters['store_id'])) {
+            $storeIds = $this->apiHelper->getDataFromFilterCondition($filters['store_id']);
+            $this->storeIds = $storeIds;
+            unset($filters['store_id']);
+        }
+        if (isset($filters['website_id'])) {
+            $websiteIds = $this->apiHelper->getDataFromFilterCondition($filters['website_id']);
+            if (!$this->storeIds) {
+                foreach ($websiteIds as $websiteId) {
+                    $storeIds = $this->getStoreIds($websiteId);
+                    foreach ($storeIds as $id) {
+                        $this->storeIds[] = $id;
+                    }
+                }
             }
         }
-        return $storeIds;
+
+        if (!$this->storeIds) {
+            $this->storeIds = $this->getStoreIds();
+        }
+
+        return $filters;
     }
 }
